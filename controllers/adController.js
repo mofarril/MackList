09ee43e8115a63a08940a4b1d0716f316a7d8ad2
Atif Rabbani <atifrabbani1@yahoo.com>
2019-11-30 13:56:40 -0500

    Added routes, controllers and models

---
+++ controllers/adController.js
@@ -0,0 +1,31 @@
+const db = require("../models");
+
+// Defining methods for the adController
+module.exports = {
+  findAll: function(req, res) {
+    db.Ad
+      .find(req.query)
+     // .sort({ date: -1 })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  findById: function(req, res) {
+    db.Ad
+      .find({ _id: req.params.id })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  create: function(req, res) {
+    db.Ad
+      .create(req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  remove: function(req, res) {
+    db.Ad
+      .findById({ _id: req.params.id })
+      .then(dbModel => dbModel.remove())
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  }
+};
