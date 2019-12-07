1c64933e831cce590726abe9e59beae95dc35520
Carlo Ortega <cfor0@outlook.com>
2019-12-07 10:10:08 -0500

    removed sidevar button

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -18,49 +18,41 @@ class Sidebar extends Component {
         return (
             <div className="row">
                 <div className="col-lg-3">
-                    <div className="pos-f-t">
-                        <button className="navbar-toggler bg-primary" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent1" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
-
-                        <div className="collapse" id="navbarToggleExternalContent1">
-                            <nav className="nav flex-column bg-dark">
-                                <a className="nav-link active" href="#collapseExample">Deparments</a>
-                                <a className="btn btn-primary" data-toggle="collapse" href="#cars" role="button" aria-expanded="false" aria-controls="collapseExample">
-                                    Cars </a>
-                                <div className="collapse" id="cars">
-                                    <div className="card card-body">
-                                        <a className="nav-link text-primary" href="#lowPrice" onClick={this.lowTohigh}>lowTohigh</a>
-                                        <a className="nav-link text-primary" href="#highPrice">highPrice</a>{}
-                                    </div>
-                                </div>
-                                <a className="nav-link" href="#">Deparments</a>
-                                <a className="btn btn-primary" data-toggle="collapse" href="#collapseExample2" role="button" aria-expanded="false" aria-controls="collapseExample">
-                                    Category </a>
-                                <div className="collapse" id="collapseExample2">
-                                    <div className="card card-body">
-                                        links of categories
-                                </div>
-                                </div>
-                                <a className="nav-link" href="#">Deparments</a>
-                                <a className="btn btn-primary" data-toggle="collapse" href="#collapseExample3" role="button" aria-expanded="false" aria-controls="collapseExample">
-                                    Category </a>
-                                <div className="collapse" id="collapseExample3">
-                                    <div className="card card-body">
-                                        links of categories
-                                </div>
-                                </div>
-                                <a className="nav-link disabled" href="#" tabIndex="-1" aria-disabled="true">Deparments</a>
-                                <a className="btn btn-primary" data-toggle="collapse" href="#collapseExample4" role="button" aria-expanded="false" aria-controls="collapseExample">
-                                    Category </a>
-                                <div className="collapse" id="collapseExample4">
-                                    <div className="card card-body">
-                                        links of categories
-                                </div>
-                                </div>
-                            </nav>
+                    <nav className="nav flex-column bg-dark">
+                        <a className="nav-link active" href="#collapseExample">Deparments</a>
+                        <a className="btn btn-primary" data-toggle="collapse" href="#cars" role="button" aria-expanded="false" aria-controls="collapseExample">
+                            Cars </a>
+                        <div className="collapse" id="cars">
+                            <div className="card card-body">
+                                <a className="nav-link text-primary" href="#lowPrice" onClick={this.lowTohigh}>lowTohigh</a>
+                                <a className="nav-link text-primary" href="#highPrice">highPrice</a>{}
+                            </div>
                         </div>
-                    </div>
+                        <a className="nav-link" href="#">Deparments</a>
+                        <a className="btn btn-primary" data-toggle="collapse" href="#collapseExample2" role="button" aria-expanded="false" aria-controls="collapseExample">
+                            Category </a>
+                        <div className="collapse" id="collapseExample2">
+                            <div className="card card-body">
+                                links of categories
+                                </div>
+                        </div>
+                        <a className="nav-link" href="#">Deparments</a>
+                        <a className="btn btn-primary" data-toggle="collapse" href="#collapseExample3" role="button" aria-expanded="false" aria-controls="collapseExample">
+                            Category </a>
+                        <div className="collapse" id="collapseExample3">
+                            <div className="card card-body">
+                                links of categories
+                                </div>
+                        </div>
+                        <a className="nav-link disabled" href="#" tabIndex="-1" aria-disabled="true">Deparments</a>
+                        <a className="btn btn-primary" data-toggle="collapse" href="#collapseExample4" role="button" aria-expanded="false" aria-controls="collapseExample">
+                            Category </a>
+                        <div className="collapse" id="collapseExample4">
+                            <div className="card card-body">
+                                links of categories
+                                </div>
+                        </div>
+                    </nav>
                 </div>
                 {/* <div id="lowPrice">
                     jsx of lowPrice function here
