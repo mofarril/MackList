960e52d7ef6bea3852349f6d2f720d5f0c46071f
Carlo Ortega <cfor0@outlook.com>
2019-12-09 20:51:42 -0500

    added scroll cities

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -6,6 +6,14 @@ import cityState from "../utils/cityState"
 
 
 class Sidebar extends Component {
+
+    state = {
+        city: []
+    }
+
+    componentDidMount = () => {
+        console.log("Hello 12" + JSON.stringify(cityState[0].city))
+    }
     lowTohigh = () => {
         Axios.get("/api/ads/lowTohigh")
             .then(response => {
@@ -35,25 +43,23 @@ class Sidebar extends Component {
                             Cars </a>
                         <div className="collapse" id="cars">
                             <div className="card card-body">
-                                <div class="form-group">
+                                <div className="form-group">
                                     <label for="FormControlSelect">City, State</label>
-                                    <select multiple class="form-control" id="FormControlSelect">
-                                        <option>1</option>
-                                        <option>2</option>
-                                        <option>3</option>
-                                        <option>4</option>
-                                        <option>5</option>
+                                    <select multiple className="form-control" id="FormControlSelect">
+                                        {cityState.map(ele => {
+                                        return <option>{ele.city}</option>
+                                        })}
                                     </select>
                                 </div>
-                                <div class="form-check">
-                                    <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" onClick={this.lowTohigh}></input>
-                                    <label class="form-check-label" for="exampleRadios1">
+                                <div className="form-check">
+                                    <input className="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" onClick={this.lowTohigh}></input>
+                                    <label className="form-check-label" for="exampleRadios1">
                                         <a className="text-primary">lowTohigh</a>
                                     </label>
                                 </div>
                                 <div className="form-check">
-                                    <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2" onClick={this.highTolow}></input>
-                                    <label class="form-check-label" for="exampleRadios2">
+                                    <input className="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2" onClick={this.highTolow}></input>
+                                    <label className="form-check-label" for="exampleRadios2">
                                         <a className="text-primary">highTolow</a>
                                     </label>
                                 </div>
@@ -64,7 +70,7 @@ class Sidebar extends Component {
                             Category </a>
                         <div className="collapse" id="collapseExample2">
                             <div className="card card-body">
-                                <div class="form-group">
+                                <div className="form-group">
                                     <label for="FormControlSelect">City, State</label>
                                     <select multiple class="form-control" id="FormControlSelect">
                                         <option>1</option>
--- client/src/utils/cityState.js
+++ client/src/utils/cityState.js
@@ -1,38 +1,49 @@
 const cityState = [
     {
+        id: 1,
         city: "Sitka",
         state: "Alaska"
     },
     {
+        id: 2,
         city: "Juneau",
         state: "Alaska"
     },
     {
+        id: 3,
         city: "Wrangell",
         state: "Alaska"
     },
     {
+        id: 4,
         city: "Anchorage",
         state: "Alaska"
     },
     {
+        id: 5,
         city: "Huntsville",
         state: "Alabama"
     },
     {
+        id: 6,
         city: "Montgomery",
         state: "Alabama"
     },
     {
+        id: 7,
         city: "Birmingham",
         state: "Alabama"
     },
     {
+        id: 8,
         city: "Moblie",
         state: "Alabama"
     },
     {
+        id: 9,
         city: "Dothan",
         state: "Alabama"
     }
 ]
+
+export default cityState
